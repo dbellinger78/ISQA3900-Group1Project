29873e3802f62fad1ebca10fc4ece5f8b99fb279
ajsautter <andrewsautter54@gmail.com>
2021-12-13 12:51:52 -0600

    test push

---
+++ tests/test1.py
@@ -0,0 +1,48 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+
+class ll_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_ll(self):
+        user = "dustin@mail.com"
+        pwd = "play2win"
+
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://127.0.0.1:8000/")
+        driver.get("http://127.0.0.1:8000/account/login")
+        elem = driver.find_element_by_id("login-username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_id("login-pwd")
+        elem.send_keys(pwd)
+        time.sleep(3)
+        elem.send_keys(Keys.RETURN)
+        driver.get("http://127.0.0.1:8000")
+        time.sleep(3)
+        #assert "Logged in"
+        try:
+            # attempt to find the 'Logout' button - if found, logged in
+           elem = driver.find_element_by_xpath("/html/body/header/nav/div/div[1]/a[2]/div")
+
+           assert True
+
+        except NoSuchElementException:
+            self.fail("Login Failed - user may not exist")
+            assert False
+
+        time.sleep(3)
+
+
+def tearDown(self):
+    self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test2.py
@@ -0,0 +1,53 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+
+class ll_ATS(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome()
+
+    def test_ll(self):
+        user = "testuser@mail.com"
+        pwd = "test123"
+
+        driver = self.driver
+        driver.maximize_window()
+        driver.get("http://127.0.0.1:8000/admin/")
+        time.sleep(3)
+        elem = driver.find_element_by_id("id_username")
+        elem.send_keys(user)
+        elem = driver.find_element_by_id("id_password")
+        elem.send_keys(pwd)
+        time.sleep(3)
+        elem.send_keys(Keys.RETURN)
+        driver.get("http://127.0.0.1:8000/admin/store/product/")
+        time.sleep(3)
+        driver.get("http://127.0.0.1:8000/admin/store/product/2/change")
+        #assert "Logged in"
+        time.sleep(3)
+        driver.get("http://127.0.0.1:8000/admin/store/product/2/delete")
+        try:
+            # attempt to find the 'Logout' button - if found, logged in
+           elem = driver.find_element_by_xpath("//*[@id='content']/form/div/input[2]").click()
+
+
+
+           assert True
+
+        except NoSuchElementException:
+            self.fail("Login Failed - user may not exist")
+            assert False
+
+        time.sleep(3)
+
+
+def tearDown(self):
+    self.driver.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
